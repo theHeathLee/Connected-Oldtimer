56e9fe871947963d3d0fe3b7b5f0494331ca397b
heath <heath.lee.ee@gmail.com>
2019-04-08 23:41:45 +0200

    screen updates from gps speed

--- ConnectedOldtimerCode/src/ConnectedOldtimerCode.cpp
+++ ConnectedOldtimerCode/src/ConnectedOldtimerCode.cpp
@@ -12,11 +12,12 @@ SYSTEM_THREAD(ENABLED);
 #include "Serial4/Serial4.h"
 #include "Serial5/Serial5.h"
 #include "../lib/TinyGPS++/src/TinyGPS++.h"
+//#include "Serial1/Serial1.h"
 
 
 TinyGPSPlus gps;
 static const uint32_t GPSBaud = 9600;
-double speed =0;
+uint8_t speed =0;
 CANChannel can(CAN_D1_D2);
 uint16_t motorTemperature = 0;
 uint16_t motorRPM = 0;
@@ -29,11 +30,13 @@ void setup() {
   can.addFilter(0x100, 0x7FF);
 
   Serial.begin(9600); //usb debugging
-  Serial4.begin(9600); // uart for nextion
+  Serial4.begin(9600); // uart for nextion c2 & c3
+  Serial1.blockOnOverrun(true);
   Serial5.begin(GPSBaud); // uart for GPS
+  Serial1.begin(GPSBaud); // 
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
-  Serial.print("started");
+  //Serial.print("started");
 
 }
 
@@ -48,16 +51,29 @@ while (Serial5.available() > 0)
 
 
   // To blink the LED, first we'll turn it on...
-
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
 
   // We'll leave it on for 1 second...
+  //collect speed
+
+  if (gps.location.isValid()) {
+    speed = gps.speed.kmph();
+    Serial.println(speed);
+  }
+  else {
+    speed = 10; // we know that gps is not valid/
+    Serial.println("speed invalid");
+  }
+
+  Serial.println("counting");
   delay(500);
-  speed = gps.speed.kmph();
-  Serial.print(speed);
+  //speed = gps.speed.kmph();
+  //Serial.print(speed);
   // Then we'll turn it off...
   digitalWrite(led1, LOW);
   digitalWrite(led2, LOW);
-  Serial4.printf("n0.val=66");
+  Serial4.printf("n0.val=");
   Serial4.print(speed);
   Serial4.write(0xff);
   Serial4.write(0xff);
@@ -72,7 +88,7 @@ while (Serial5.available() > 0)
 
 
 
-//gps info 
+//gps info for debugging
 
 void displayInfo()
 {
@@ -105,17 +121,17 @@ void displayInfo()
   Serial.print(F(" "));
   if (gps.time.isValid())
   {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
+    // if (gps.time.hour() < 10) Serial.print(F("0"));
+    // Serial.print(gps.time.hour());
+    // Serial.print(F(":"));
+    // if (gps.time.minute() < 10) Serial.print(F("0"));
+    // Serial.print(gps.time.minute());
+    // Serial.print(F(":"));
+    // if (gps.time.second() < 10) Serial.print(F("0"));
+    // Serial.print(gps.time.second());
+    // Serial.print(F("."));
+    // if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    // Serial.print(gps.time.centisecond());
 
     //Serial.print("Speed= ");
 
