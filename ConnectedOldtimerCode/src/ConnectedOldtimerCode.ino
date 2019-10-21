df809e2826a174e608718df270facd789c79e116
heath <heath.lee.ee@gmail.com>
2019-10-21 09:59:41 +0200

    removed hdop debug to display

--- ConnectedOldtimerCode/src/ConnectedOldtimerCode.ino
+++ ConnectedOldtimerCode/src/ConnectedOldtimerCode.ino
@@ -180,8 +180,7 @@ void updateDisplay() {
 
   // sends data to display
   Serial4.printf("n0.val=");
-  // Serial4.print(nextionSpeed);
-  Serial4.print(gps.hdop.value());
+  Serial4.print(nextionSpeed);
   // next 3 writes must be made for the Nextion to accept the update
   Serial4.write(0xff);
   Serial4.write(0xff);
