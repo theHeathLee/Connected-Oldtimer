4912931d5e322c9dcd26a5fcd348c5aba2bd4e5f
heath <heath.lee.ee@gmail.com>
2020-12-24 14:49:45 +0100

    moved user c file into main isotp file. Compiles Successfully :)

--- ConnectedOldtimerCode/lib/isotp-c/src/isotp.cpp
+++ ConnectedOldtimerCode/lib/isotp-c/src/isotp.cpp
@@ -1,7 +1,8 @@
 #include <stdint.h>
 #include "assert.h"
 #include "isotp.h"
-#include "isotp_user.h"
+#include <Particle.h>
+//#include "isotp_user.h"
 
 ///////////////////////////////////////////////////////
 ///                 STATIC FUNCTIONS                ///
@@ -515,3 +516,29 @@ void isotp_poll(IsoTpLink *link) {
     return;
 }
 
+
+//user (moved from isotp_user)
+   int  isotp_user_send_can(const uint32_t arbitration_id,
+                             const uint8_t* data, const uint8_t size) {
+        CANChannel can(CAN_C4_C5);
+        CANMessage messageIsoTp;
+        messageIsoTp.id = arbitration_id;
+        //messageIsoTp.data = *data;
+        //uint8_t txData[8];
+        //uint8_t txData[8]= {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+        *messageIsoTp.data = *data;
+        messageIsoTp.size = size;
+        can.transmit(messageIsoTp); 
+        
+        return 1;
+    }
+
+    /* required, return system tick, unit is millisecond */
+    uint32_t isotp_user_get_ms(void) {
+        return (uint32_t)GetSystem1MsTick();
+    }
+
+void isotp_user_debug(const char* message, ...)
+{
+    //empty function
+}
